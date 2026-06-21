726efe390a5b7f54c691847345643b4fdd3a4fb1
Swati Gavali <gavaliswati1993@gmail.com>
2026-06-21 18:28:31 +0530

    Create Database_Schema.sql

---
+++ 17_Ecommerce_SQL_Project/Database_Schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE EcommerceDB;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(100),
+    Email VARCHAR(100),
+    City VARCHAR(50)
+);
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DECIMAL(10,2)
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
