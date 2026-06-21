e104683128d309d99be35e15c460f2e22525868f
Swati Gavali <gavaliswati1993@gmail.com>
2026-06-21 18:29:28 +0530

    Create Sample_Data.sql

---
+++ 17_Ecommerce_SQL_Project/Sample_Data.sql
@@ -0,0 +1,17 @@
+INSERT INTO Customers
+VALUES
+(1,'Amit Sharma','amit@gmail.com','Mumbai'),
+(2,'Priya Patil','priya@gmail.com','Pune'),
+(3,'Rahul Verma','rahul@gmail.com','Bangalore');
+
+INSERT INTO Products
+VALUES
+(101,'Laptop','Electronics',55000),
+(102,'Headphones','Electronics',2500),
+(103,'Office Chair','Furniture',7000);
+
+INSERT INTO Orders
+VALUES
+(1001,1,'2025-01-10'),
+(1002,2,'2025-01-15'),
+(1003,3,'2025-01-20');
